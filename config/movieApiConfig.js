b52622e309e044d8557d7b77ec819ca79c2eb010
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2023-11-28 14:13:10 +0100

    cors setup, tmdb api setup

--- config/movieApiConfig.js
+++ config/movieApiConfig.js
@@ -1,3 +1,5 @@
 module.exports = {
-    apiKey: '',//add api key here
+    apiKey: '978c227f75cf1305f5de177f3773f57e',
+    baseUrl: 'https://api.themoviedb.org/3',
+    APIReadAccessToken: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzhjMjI3Zjc1Y2YxMzA1ZjVkZTE3N2YzNzczZjU3ZSIsInN1YiI6IjY1NjVkMjYxODlkOTdmMDEzOGZlZGM4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7aTb8ZOlXp6n2eh0TyCrm6dEdC8d7Fer2VasJPKVydU"
   };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:3000' }));//add origin when deployed
+
 const PORT = process.env.PORT || 5001;
 const movieRoutes = require('./routes/movieRoutes');
 
-app.use('/movie', movieRoutes);
+app.use('/movies', movieRoutes);
 app.use(express.static('build'));
 
 app.listen(PORT, () => {
