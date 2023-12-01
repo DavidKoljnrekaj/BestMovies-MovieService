1eec5ad635cfff5ce5192a5d58637ce44bdbf835
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2023-12-01 14:25:55 +0100

    CI/CD setup

--- src/server.js
+++ src/server.js
@@ -3,7 +3,21 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));//add origin when deployed
+const allowedOrigins = ['http://localhost:3000', 'http://localhost'];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const PORT = process.env.PORT || 5001;
 const movieRoutes = require('./routes/movieRoutes');
