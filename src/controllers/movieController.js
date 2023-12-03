9eb4086d71482e5aaff9a0843213573a3ca5e1f8
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2023-12-03 21:18:30 +0100

    routes,controllers and service methods for filtering and sorting movies

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -2,8 +2,8 @@ const movieService = require('../services/movieService');
 
 exports.searchMovies = async (req, res, next) => {
   try {
-    const { query } = req.query;
-    const result = await movieService.searchMovies(query);
+    const { query, adult, page } = req.query;
+    const result = await movieService.searchMovies(query, adult == 'true', Number(page));
     res.json(result);
   } catch (error) {
     next(error);
@@ -20,4 +20,78 @@ exports.getMovieDetails = async (req, res, next) => {
   }
 };
 
+exports.getTrendingMoviesToday = async (req, res) => {
+  try {
+    const movies = await movieService.getTrendingMoviesToday();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+};
+
+exports.getTrendingMoviesThisWeek = async (req, res) => {
+  try {
+    const movies = await movieService.getTrendingMoviesThisWeek();
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+};
+
+exports.discoverMovies = async (req, res, next) => {
+  try {
+    const { genres, year, rating, sortBy, adult, minVoteCount } = req.query;
+    const result = await movieService.discoverMovies(
+      genres ? genres.split(',') : null,
+      year,
+      rating,
+      sortBy,
+      adult === 'true',
+      minVoteCount
+    );
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getNowPlayingMovies = async (req, res, next) => {
+  try {
+    const { page } = req.query;
+    const result = await movieService.getNowPlayingMovies(Number(page));
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getPopularMovies = async (req, res, next) => {
+  try {
+    const { page } = req.query;
+    const result = await movieService.getPopularMovies(Number(page));
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getTopRatedMovies = async (req, res, next) => {
+  try {
+    const { page } = req.query;
+    const result = await movieService.getTopRatedMovies(Number(page));
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getUpcomingMovies = async (req, res, next) => {
+  try {
+    const { page } = req.query;
+    const result = await movieService.getUpcomingMovies(Number(page));
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -5,6 +5,15 @@ const router = express.Router();
 
 router.get('/search', movieController.searchMovies);
 router.get('/details/:movieId', movieController.getMovieDetails);
+router.get('/trending/today', movieController.getTrendingMoviesToday);
+router.get('/trending/week', movieController.getTrendingMoviesThisWeek);
+router.get('/discover', movieController.discoverMovies);
+router.get('/movies/now_playing', movieController.getNowPlayingMovies);
+router.get('/movies/popular', movieController.getPopularMovies);
+router.get('/movies/top_rated', movieController.getTopRatedMovies);
+router.get('/movies/upcoming', movieController.getUpcomingMovies);
+
+
 
 
 module.exports = router;
