d053d4e57a591889432d2ae54f89ef98a6815959
DavidKoljnrekaj <koljnrekajdavid@gmail.com>
2023-12-04 17:15:56 +0100

    routes testing

--- src/__tests__/routes.test.js
+++ src/__tests__/routes.test.js
@@ -10,7 +10,7 @@ describe('Movie Routes', () => {
     it('should search movies', async () => {
       const res = await request(app)
         .get('/search')
-        .query({ query: 'Tenet' })
+        .query({ query: 'Tenet' , adult: true, page: 1})
         .expect('Content-Type', /json/)
         .expect(200);
   
@@ -26,4 +26,63 @@ describe('Movie Routes', () => {
       expect(res.body).toHaveProperty('id');
       expect(res.body.id).toBe(577922);
     });
+
+    it('should get trending movies this week', async () => {
+      const res = await request(app)
+        .get('/trending/week')
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(res.body).toHaveProperty('results');
+    });
+
+    it('should get trending movies today', async () => {
+      const res = await request(app)
+        .get('/trending/today')
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(res.body).toHaveProperty('results');
+    });
+
+    it('should get popular movies', async () => {
+      const res = await request(app)
+        .get('/popular')
+        .query({ page: 1})
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(res.body).toHaveProperty('results');
+    });
+
+    it('should get upcoming movies', async () => {
+      const res = await request(app)
+        .get('/upcoming')
+        .query({ page: 1})
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(res.body).toHaveProperty('results');
+    });
+
+    it('should get playing now movies', async () => {
+      const res = await request(app)
+        .get('/now_playing')
+        .query({ page: 1})
+        .expect('Content-Type', /json/)
+        .expect(200);
+  
+      expect(res.body).toHaveProperty('results');
+    });
+
+    it('should get top rated movies', async () => {
+      const res = await request(app)
+        .get('/top_rated')
+        .query({ page: 1})
+        .expect(200);
+  
+      expect(res.body).toHaveProperty('results');
+    });
+
+
   });
